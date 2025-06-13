f74eb591c6e4c1aad903ec8a0b9ebb9588f534f9
Sharkvy <mateokristic2005@gmail.com>
2025-06-13 11:50:21 +0200

    small update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<Space-animation>/', // Replace <YOUR_REPOSITORY_NAME>
+  base: '/Space-animation/',
 })
